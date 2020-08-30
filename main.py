c1c410420d377becd0b15eea9587f32afc445866
“Imad <imadnajmi9@gmail.com>
2020-08-30 14:07:07 +0100

    finish code

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
 import requests
 import csv
+import logging
+from getpass import getpass
+import sys
 
-url = "https://api.github.com/repositories"
-r = requests.get(url)
+logging.basicConfig(level = logging.DEBUG)
 
-print(r.json())
+username = input('*Enter your username : \t')
+password = getpass()
+
+orgName = input("*Enter your organization name : \t")
+repoName = input("Enter your repository name : \t")
+
+while orgName == '':
+	orgName = input('Enter the org name : \t')
+
+if repoName:
+	url = "https://api.github.com/orgs/{}/repos/{}".format(orgName, repoName)
+
+
+url = "https://apsi.github.com/orgs/{}/repos".format(orgName)
+headers = {'Accept':'application/vnd.github.v3+json','Content-Type':'application/json' }
+
+r = requests.get(url, headers = headers, auth=(username,password))
+
+
+status =  r.status_code
+
+if status == 200:
+	repos = r.json()
+	f = open('data.csv', 'w', newline='')
+	csv_writer = csv.writer(f)
+	csv_writer.writerow(['id', 'name', 'url', 'language', 'owner', 'created_at'])
+
+	for item in repos:
+		csv_writer.writerow([item['id'], item['name'], item['url'], item['language'], item['owner']['login'], item['created_at']])
+	f.close()
+	logging.debug('Process finished successufly !')
+else:
+	logging.error(r.text)
+sys.exit()
